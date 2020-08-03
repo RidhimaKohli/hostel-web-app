436af9318eede4b34c6a8d2933e89d24b5cd3156
rajkotiya <69031687+rajkotiya@users.noreply.github.com>
2020-08-03 22:25:06 +0530

    Update models.py

--- hostel_project/hostel_webapp/models.py
+++ hostel_project/hostel_webapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+# Create your models her
+class complain(models.Model):
+    name = models.CharField(max_length=100)
+    rollno = models.CharField(max_length=12)
+    img = models.ImageField(upload_to='pics')
+    desc = models.TextField()
+    date = models.DateField()
+
