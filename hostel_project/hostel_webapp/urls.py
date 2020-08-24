fd6acdf54c5c96085c249e80752ccd588b502d47
Saumya0206 <saumyaaaaa0206@gmail.com>
2020-08-24 22:06:10 +0530

    integrated

--- hostel_project/hostel_webapp/urls.py
+++ hostel_project/hostel_webapp/urls.py
@@ -1,5 +1,11 @@
 from django.urls import path
 from . import views
+
+
 urlpatterns = [
-    path('', views.home, name='hostel_webapp-home'),
+    path('', views.index, name='index'),
+    path('about.html', views.about, name='about'),
+    path('login.html', views.login, name='login'),
+    path('complaint.html', views.complaint, name='complaint'),
+    path('g4.html', views.g4, name='g4'),
 ]
